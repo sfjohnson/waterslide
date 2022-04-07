25ee8e4ee6eaa8dc0b034bc9972e209fc7f54a7d
Sam Johnson <sam@iroha.is>
2022-04-07 16:39:11 +1000

    Potential fix for openssl homebrew path issue

--- macos10.mk
+++ macos10.mk
@@ -5,9 +5,9 @@ PROTOC = bin/protoc
 PROTOCFLAGS = --cpp_out=.
 # CFLAGS = -g3 -fno-omit-frame-pointer -fsanitize=address
 # LIBS = -g3 -fno-omit-frame-pointer -fsanitize=address
-CFLAGS = -std=c17 -O3 -fstrict-aliasing -pedantic -pedantic-errors -Wall -Wextra -I./include -I./include/deps -I./include/deps/ck -I/usr/local/Cellar/openssl@3/3.0.1/include
-CPPFLAGS = -std=c++17 -O3 -fstrict-aliasing -Wno-gnu-anonymous-struct -Wno-nested-anon-types -pedantic -pedantic-errors -Wall -Wextra -I./include -I./include/deps -I./include/deps/ck -I/usr/local/Cellar/openssl@3/3.0.1/include
-LDFLAGS = -Llib/macos10 -L/usr/local/Cellar/openssl@3/3.0.1/lib -pthread
+CFLAGS = -std=c17 -O3 -fstrict-aliasing -pedantic -pedantic-errors -Wall -Wextra -I./include -I./include/deps -I./include/deps/ck -I/usr/local/opt/openssl@3/include
+CPPFLAGS = -std=c++17 -O3 -fstrict-aliasing -Wno-gnu-anonymous-struct -Wno-nested-anon-types -pedantic -pedantic-errors -Wall -Wextra -I./include -I./include/deps -I./include/deps/ck -I/usr/local/opt/openssl@3/include
+LDFLAGS = -Llib/macos10 -L/usr/local/opt/openssl@3/lib -pthread
 LIBS = -lstdc++ -lm -lz -lopus -lportaudio -lr8brain -lraptorq -lck -lssl -lcrypto -luwebsockets -lprotobuf-lite -framework CoreAudio -framework AudioUnit -framework AudioToolbox -framework CoreServices
 
 TARGET = waterslide-macos10
