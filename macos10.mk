3995f651365400da4175996f84ddc125cebef099
Sam Johnson <samj@grumpysailor.com>
2023-02-01 11:52:34 +1100

    Updated ASan flags in Makefile for LTO.

--- macos10.mk
+++ macos10.mk
@@ -4,12 +4,14 @@ CC = $(LLVM_PATH)/bin/clang
 CPP = $(LLVM_PATH)/bin/clang
 PROTOC = bin/protoc
 PROTOCFLAGS = --cpp_out=.
-# CFLAGS = -g3 -fno-omit-frame-pointer -fsanitize=address
-# LIBS = -g3 -fno-omit-frame-pointer -fsanitize=address
 CFLAGS = -std=c17 -O3 -flto -fstrict-aliasing -pedantic -pedantic-errors -Wall -Wextra -I./include -I./include/deps -I./include/deps/ck -I/usr/local/opt/openssl@3/include
 CPPFLAGS = -std=c++17 -O3 -flto -fstrict-aliasing -Wno-gnu-anonymous-struct -Wno-nested-anon-types -pedantic -pedantic-errors -Wall -Wextra -I./include -I./include/deps -I./include/deps/ck -I/usr/local/opt/openssl@3/include
 LDFLAGS = -Llib/macos10 -L/usr/local/opt/openssl@3/lib -pthread -flto
 LIBS = -lstdc++ -lm -lz -lopus -lportaudio -lr8brain -lraptorq -lck -lssl -lcrypto -luwebsockets -lprotobuf-lite -lboringtun -framework CoreAudio -framework AudioUnit -framework AudioToolbox -framework CoreServices -framework Security
+
+# CFLAGS += -g3 -fno-omit-frame-pointer -fsanitize=address
+# CPPFLAGS += -g3 -fno-omit-frame-pointer -fsanitize=address
+# LDFLAGS += -fsanitize=address "-Wl,-object_path_lto,lto.o"
 
 TARGET = waterslide-macos10
 PROTOBUFS = init-config.proto monitor.proto
